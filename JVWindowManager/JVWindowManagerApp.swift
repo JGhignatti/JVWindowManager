59e16563510b7df65040a0eb58c260924e091f22
JGhignatti <ghignatti.joao@gmail.com>
2025-04-15 17:04:45 -0300

    Initial Commit

---
+++ JVWindowManager/JVWindowManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JVWindowManagerApp.swift
+//  JVWindowManager
+//
+//  Created by João Ghignatti on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct JVWindowManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
